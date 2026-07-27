46a7dc2508c79861fae7a5340daabf787f03ed67
fth17172 <fth17172@gmail.com>
2026-07-27 18:49:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// Arka Planda Çalışan Servis
 self.addEventListener('install', (e) => {
     self.skipWaiting();
 });
@@ -7,24 +6,7 @@ self.addEventListener('activate', (e) => {
     return self.clients.claim();
 });
 
-// Arka Planda Push Bildirimi Geldiğinde
-self.addEventListener('push', (e) => {
-    const data = e.data ? e.data.json() : {};
-
-    const options = {
-        body: data.body || 'Seni arıyor!',
-        icon: 'https://cdn-icons-png.flaticon.com/512/3059/3059502.png',
-        badge: 'https://cdn-icons-png.flaticon.com/512/3059/3059502.png',
-        vibrate: [200, 100, 200, 100, 200], // Titreşim efekti
-        data: { url: '/' }
-    };
-
-    e.waitUntil(
-        self.registration.showNotification(data.title || 'Gelen Arama!', options)
-    );
-});
-
-// Bildirime Tıklandığında Uygulamayı Aç
+// Bildirime tıklanınca uygulamayı öne getir
 self.addEventListener('notificationclick', (e) => {
     e.notification.close();
     e.waitUntil(
